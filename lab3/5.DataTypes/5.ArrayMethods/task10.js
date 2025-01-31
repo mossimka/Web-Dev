d02e691169d70e292f237440047ba36655756dc7
mossimka <maksimsarsekeev@gmail.com>
2025-01-31 15:39:09 +0500

    todo list

--- lab3/5.DataTypes/5.ArrayMethods/task10.js
+++ lab3/5.DataTypes/5.ArrayMethods/task10.js
@@ -1,7 +1,7 @@
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
-  }
+}
   
-  let arr = [1, 2, 3];
-  shuffle(arr);
-  alert(arr);
+let arr = [1, 2, 3];
+shuffle(arr);
+alert(arr);
