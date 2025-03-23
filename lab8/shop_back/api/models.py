8307d2d89544053fc0f04c39844a9fec98c34891
mossimka <maksimsarsekeev@gmail.com>
2025-03-23 20:15:31 +0500

    classes

---
+++ lab8/shop_back/api/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField()
+    image = models.URLField()
+    images = models.JSONField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "price": self.price,
+            "description": self.description,
+            "count": self.count,
+            "is_active": self.is_active,
+            "image": self.image,
+            "images": self.images,
+            "category": self.category
+        }
