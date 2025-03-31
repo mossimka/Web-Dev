430e7dc392b31572529fea011af39a285afd96fd
mossimka <maksimsarsekeev@gmail.com>
2025-03-31 17:28:17 +0500

    lab9 completed

--- lab9/hh_back/api/models.py
+++ lab9/hh_back/api/models.py
@@ -13,7 +13,7 @@ class Vacancy(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     salary = models.FloatField()
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="company")
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="vacancies")
 
     def __str__(self):
         return f"{self.name} - {self.company} - {self.salary}"
--- lab9/hh_back/api/views.py
+++ lab9/hh_back/api/views.py
@@ -3,8 +3,8 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Company
-from .serializers import CompanySerializer
+from .models import Company, Vacancy
+from .serializers import CompanySerializer, VacancySerializer
 
 
 # Create your views here.
@@ -23,22 +23,89 @@ def get_all_companies(request):
         return JsonResponse(serializer.errors, status=400)
 
 
+@csrf_exempt
+def get_company(request, id):
+    try:
+        company = Company.objects.get(pk=id)
+    except Company.DoesNotExist:
+        return JsonResponse({'error': 'Company not found'}, status=404)
 
-def get_company():
-    return None
+    if request.method == 'GET':
+        serializer = CompanySerializer(company)
+        return JsonResponse(serializer.data, status=200)
 
+    elif request.method == 'PUT':
+        new_data = json.loads(request.body)
+        serializer = CompanySerializer(instance=company, data=new_data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        company.delete()
+        return JsonResponse({'message': 'Company deleted'}, status=200)
 
-def get_company_vacancies():
-    return None
+@csrf_exempt
+def get_company_vacancies(request, id):
+    try:
+        company =  Company.objects.get(pk=id)
+    except Company.DoesNotExist:
+        return JsonResponse({'error': 'Company not found'}, status=404)
 
+    if request.method == 'GET':
+        vacancies = company.vacancies.all()
+        serializer = VacancySerializer(vacancies, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
-def get_all_categories():
-    return None
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        data['company'] = id
+        serializer = VacancySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
+@csrf_exempt
+def get_all_vacancies(request):
+    if request.method == 'GET':
+        vacancies = Vacancy.objects.all()
+        serializer = VacancySerializer(vacancies, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
-def get_category():
-    return None
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        serializer = VacancySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
+@csrf_exempt
+def get_vacancy(request, id):
+    try:
+        vacancy = Vacancy.objects.get(pk=id)
+    except Vacancy.DoesNotExist:
+        return JsonResponse({'error': 'Vacancy not found'}, status=404)
 
-def get_category_products():
-    return None
+    if request.method == 'GET':
+        serializer = VacancySerializer(vacancy)
+        return JsonResponse(serializer.data, status=200)
+
+    elif request.method == 'PUT':
+        new_data = json.loads(request.body)
+        serializer = VacancySerializer(instance=vacancy, data=new_data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        vacancy.delete()
+        return JsonResponse({'message': 'Vacancy deleted'}, status=200)
+
+def get_top_vacancies(request):
+    if request.method == 'GET':
+        top_vacancies = Vacancy.objects.order_by('-salary')[:10]  # Top 10 highest salaries
+        serializer = VacancySerializer(top_vacancies, many=True)
+        return JsonResponse(serializer.data, safe=False)
