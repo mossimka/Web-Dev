19027a8b250ae5267755056496e6670abf8cc729
mossimka <maksimsarsekeev@gmail.com>
2025-04-02 22:44:10 +0500

    angular setup

--- lab10/hh_back/api/serializers.py
+++ lab10/hh_back/api/serializers.py
@@ -11,3 +11,10 @@ class VacancySerializer(serializers.ModelSerializer):
     class Meta:
         model = Vacancy
         fields = ('id', 'name', 'description', 'salary', 'company')
+
+class CompanySerializerSimple(serializers.Serializer):
+    id = serializers.IntegerFiled()
+    name = serializers.CharField()
+    description = serializers.TextField()
+    city = serializers.CharFied()
+    address = serializers.TextField()
